8a41283fbf8b3963257a87051f0cc7260d48ecad
hongquan16101995 <66354589+hongquan16101995@users.noreply.github.com>
2022-09-09 17:39:54 +0700

    condition

---
+++ JavaScript/Condition/switch.js
@@ -0,0 +1,20 @@
+let num = 1
+switch (num) {
+    case 1:
+        document.getElementById("result").innerHTML = "Hello"
+    // break
+    case 2:
+        document.getElementById("result").innerHTML = ("Bye")
+    // break
+    case 3:
+        document.getElementById("result").innerHTML = ("Bye")
+    // break
+    case 4:
+        document.getElementById("result").innerHTML = ("Bye")
+    // break
+    case 5:
+        document.getElementById("result").innerHTML = ("Bye")
+    // break
+    default:
+        document.getElementById("result").innerHTML = ("HelloWorld")
+}
