67f8b86cc605f1ce2895e9839045864ef18dd477
hongquan16101995 <66354589+hongquan16101995@users.noreply.github.com>
2022-09-13 16:36:04 +0700

    loop

---
+++ JavaScript/Condition-2/debug.js
@@ -0,0 +1,10 @@
+let a = 10
+if (a > 50) {
+    console.log("Hello")
+} else if (a > 30) {
+    console.log("Bye")
+} else if (a > 20) {
+    console.log("Hi")
+} else {
+    console.log("Debug")
+}
---
+++ JavaScript/Loop/loop.js
@@ -0,0 +1,24 @@
+//cấu trúc for
+let i = 0
+for (; i < 10;) {
+    console.log("Đây là lần lặp thứ: " + (i) + ", giá trị của i: " + i)
+    i++
+}
+
+console.log("-----------------")
+
+//cấu trúc while
+let j = 0
+while (j > 10) {
+    console.log("Đây là lần lặp thứ: " + (j) + ", giá trị của i: " + j)
+    j++
+}
+
+console.log("-----------------")
+
+//cấu trúc do..while
+let h = 0
+do {
+    console.log("Đây là lần lặp thứ: " + (h) + ", giá trị của i: " + h)
+    h++
+} while (h > 10)
